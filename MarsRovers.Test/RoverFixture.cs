6852753fbe15068b03634e9a915cf640cb6ea586
Deepak Shrivastav <dshrivastav@affinionds.com>
2019-07-24 19:16:45 +0530

    Added Rover Class

---
+++ MarsRovers.Test/RoverFixture.cs
@@ -0,0 +1,27 @@
+using System;
+using Xunit;
+using MarsRovers;
+using FluentAssertions;
+namespace MarsRovers.Test
+{
+    public class RoverFixtures
+    {
+
+        private Rover _rover = new Rover();
+        [Fact]
+        public void Every_rover_should_have_map()
+        {
+            _rover.map.GetType().Should().Be(typeof(Map));
+        }
+        [Fact]
+        public void Every_rover_should_have_commandList()
+        {
+            _rover.commands.GetType().Should().Be(typeof(CommandList));
+        }
+        [Fact]
+        public void Every_rover_should_have_direction()
+        {
+            _rover.direction.GetType().Should().Be(typeof(Direction));
+        }
+    }
+}
---
+++ MarsRovers/Rover.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MarsRovers
+{
+    public class Rover
+    {
+        public Map map{ get; set; }
+        public CommandList commands;
+        public Direction direction;
+        public Rover()
+        {
+            map = new Map();
+            commands = new CommandList();
+            direction = new Direction();
+        }
+        public static void Main()
+        {
+
+        }
+    }
+    public class Map
+    {
+
+    }
+    public class CommandList
+    {
+
+    }
+    public class Direction
+    {
+
+    }
+}
